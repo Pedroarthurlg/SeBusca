009f7df94dea0d15cccb50cc7d9dc390b0af49e0
JoaoLuucasOL <joaolucas_deoliveira@yahoo.com.br>
2022-09-17 22:35:37 -0300

    17-09-2022 22-35

---
+++ project/settings/installed_apps.py
@@ -0,0 +1,15 @@
+INSTALLED_APPS = [
+    # Django
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Django REST
+    'rest_framework',
+    # APPS criados
+    'recipes',
+    'authors',
+    'tag',
+]

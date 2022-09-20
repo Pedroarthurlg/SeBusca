41a1f3a09e65d3ad8fff62782cad70678df19e27
JoaoLuucasOL <joaolucas_deoliveira@yahoo.com.br>
2022-09-20 05:55:34 -0300

    20-09-2022 05-55

--- project/settings/__init__.py
+++ project/settings/__init__.py
@@ -3,11 +3,14 @@ from .environment import *  # isort:skip
 from .installed_apps import *  # isort:skip
 from .middlewares import *  # isort:skip
 
+import django_heroku
+
 from .assets import *
 from .databases import *
 from .debug_toolbar import *
-from .heroku import *
 from .i18n import *
 from .messages import *
 from .security import *
 from .templates import *
+
+django_heroku.settings(locals())
